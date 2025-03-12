bb58ef18b450bba28218ab48e45798a67ec63b6d
Nguyen Huu Tuan <tuannhhe172829@fpt.edu.vn>
2025-03-12 23:32:32 +0700

    #updated: LoginUI

--- frontend/src/modules/user/components/LoginUI.tsx
+++ frontend/src/modules/user/components/LoginUI.tsx
@@ -1,7 +1,8 @@
 import {Button, Checkbox, Col, Form, FormProps, Image, Input, Layout, Row, Typography} from "antd";
 import {ILoginForm} from "../models/auth.ts";
+import betacinema from "../../../assets/betacinema.png";
 import * as React from "react";
-import {Content, Header} from "antd/es/layout/layout";
+import {Content} from "antd/es/layout/layout";
 const { Title } = Typography;
 
 const onFinish: FormProps<ILoginForm>['onFinish'] = (values) => {
@@ -13,59 +14,55 @@ const onFinishFailed: FormProps<ILoginForm>['onFinishFailed'] = (errorInfo) => {
 }
 
 const LoginForm: React.FC = () => (
-    <Layout style={{ minHeight: "100vh" }}>
-        {/* Header */}
-        <Header style={{ background: "#001529", color: "white", textAlign: "center" }}>
-            <Title level={3} style={{ color: "white", margin: 0 }}>Ant Design Grid Layout</Title>
-        </Header>
-
-        {/* Main Content */}
-        <Content style={{ padding: "24px" }}>
-            <Row gutter={[16, 16]} justify="center" style={{ maxWidth: 1200, margin: "0 auto" }}>
+    <Layout style={{ minHeight: "100vh", overflow: "hidden" }}>
+        <Content>
+            <Row gutter={[16, 16]} justify="center" align="middle" style={{ height: "100vh" }}>
                 {/* Left Column (40%) */}
-                <Col xs={24} md={10}>
+                <Col xs={24} md={10} style={{ height: "100%" }}>
                     <Image
-                        src="https://www.vietvisiontravel.com/wp-content/uploads/2017/07/cgv-vincom-ba-trieu-cinema-hanoi.jpg"
+                        src={betacinema}
                         alt="Example Image"
-                        className="w-full h-full object-cover"
+                        style={{ width: "100%", height: "100%", objectFit: "cover" }}
                     />
                 </Col>
 
                 {/* Right Column (60%) */}
-                <Col xs={24} md={14}>
-                    <Form
-                        name="basic"
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        className="w-full"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item
-                            label="Username"
-                            name="username"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
+                <Col xs={24} md={14} style={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <div style={{ width: "100%", maxWidth: "70%", textAlign: "center" }}>
+                        <Title level={3}>Sigma Cinema</Title>
+
+                        <Form
+                            name="basic"
+                            layout="vertical"
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
                         >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-                        <Form.Item name="remember" valuePropName="checked">
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                Submit
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[{ required: true, message: "Please input your username!" }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: "Please input your password!" }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item name="remember" valuePropName="checked">
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" block>
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
                 </Col>
             </Row>
         </Content>
